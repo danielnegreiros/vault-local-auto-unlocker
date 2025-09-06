0f524d927f9625f873803d7d65c012eb38c0a0ca
Daniel Negreiros Barros <daniel.negreiros.barros@kyndryl.com>
2025-09-06 17:59:20 -0300

    kubernetes

--- exporter/kubernetes.go
+++ exporter/kubernetes.go
@@ -2,11 +2,15 @@ package exporter
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"log/slog"
 	"path/filepath"
 	"vault-unlocker/conf"
 
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
@@ -53,9 +57,8 @@ func NewkubernetesClient(appCfg *conf.Exporter) (*kubernetesClient, error) {
 	return storage, nil
 }
 
-
 // RetrieveKeys implements storage.
-func (h *kubernetesClient) RetrieveKeys(ctx context.Context) error {
+func (h *kubernetesClient) ListSecrets(ctx context.Context) error {
 	secrets, err := h.Client.CoreV1().Secrets(h.Namespace).List(ctx, metav1.ListOptions{})
 	if err != nil {
 		return err
@@ -68,7 +71,49 @@ func (h *kubernetesClient) RetrieveKeys(ctx context.Context) error {
 	return nil
 }
 
-// UpdateKeys implements storage.
-func (h *kubernetesClient) UpdateKeys() {
-	panic("unimplemented")
+// ReadkSecret implements storage.
+func (h *kubernetesClient) ReadkSecret(ctx context.Context, name string) error {
+	secret, err := h.Client.CoreV1().Secrets(h.Namespace).Get(ctx, name, metav1.GetOptions{})
+	if err != nil {
+		return err
+	}
+
+	slog.Info("iterating", "secret", secret.Name, "data", secret.Data)
+
+	return nil
+}
+
+func (h *kubernetesClient) CreateSecret(ctx context.Context, name string, data map[string][]byte) (*corev1.Secret, error) {
+	secret := &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: h.Namespace,
+		},
+		Data: data,
+	}
+	return h.Client.CoreV1().Secrets(h.Namespace).Create(ctx, secret, metav1.CreateOptions{})
+}
+
+func convertToByteMap(input map[string]interface{}) (map[string][]byte, error) {
+	output := make(map[string][]byte)
+
+	for k, v := range input {
+		switch val := v.(type) {
+		case string:
+			output[k] = []byte(val)
+		case []byte:
+			output[k] = val
+		case fmt.Stringer: // things like time.Time or custom types
+			output[k] = []byte(val.String())
+		default:
+			// try JSON marshal as a fallback
+			b, err := json.Marshal(val)
+			if err != nil {
+				return nil, fmt.Errorf("cannot convert key %s: %w", k, err)
+			}
+			output[k] = b
+		}
+	}
+
+	return output, nil
 }
--- exporter/kubernetes_test.go
+++ exporter/kubernetes_test.go
@@ -23,6 +23,39 @@ exporters:
 	assert.NoError(t, err)
 
 	ctx := context.Background()
-	err = client.RetrieveKeys(ctx)
+	err = client.ListSecrets(ctx)
 	assert.NoError(t, err)
+	err = client.ReadkSecret(ctx, "grafana")
+	assert.NoError(t, err)
+}
+
+func TestCreateSecret(t *testing.T) {
+	data := `
+exporters:
+  type: kubernetes
+  kubernetes:
+    access: out-cluster
+    namespace: observability
+`
+	appCfg, err := conf.NewConfig([]byte(data))
+	assert.NoError(t, err)
+
+	client, err := NewkubernetesClient(appCfg.Exporter)
+	assert.NoError(t, err)
+
+	secretData := map[string]interface{}{
+		"username": "admin",
+		"password": "s3cr3t",
+		"enabled":  true,
+		"count":    42,
+	}
+
+	converted, err := convertToByteMap(secretData)
+	assert.NoError(t, err)
+
+	sec, err := client.CreateSecret(context.Background(), "my-secret", converted)
+	assert.NoError(t, err)
+
+	t.Logf("Created secret: %v", sec)
+
 }
